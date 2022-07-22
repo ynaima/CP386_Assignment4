a33a911898a21d27217497776e7f0b738e5d8223
ynaimo <naimoyasin14@gmail.com>
2022-07-21 23:46:17 -0600

    updating new changes

--- allocation.c
+++ allocation.c
@@ -2,21 +2,45 @@
  * github login ID2: abdullahi3bdi
  * URL:  https://github.com/ynaima/CP386_Assignment4
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// C implementation of First - Fit algorithm
-#include<stdio.h>
 
-still working on it.
 
-// Driver code
-int main()
+
+int main(void)
 {
-       //number of blocks in the memory
-      //number of processes in the input queue
+    char sent[1000] = { '\0' };
 
+    while (1) {
+        //Enter Exit to stop execution:
+        fgets(sent, 1000, stdin);
+        if (strncmp(sent, "Exit", 4) == 0) {
+            printf("You broke the loop of execution.\n");
+            break;
+        }
+        else{
+        	//firstFit(blockSize, m, processSize, n);
+			//bestFit(blockSize, m, processSize, n);
+			//worstFit(blockSize, m, processSize, n);
+			printf("One!");
+			continue;
 
-    return 0 ;
+        }
+    }
+    return 0;
 }
 
 
-//This is for testing
+
+
+
+
+
+
+
+
+
+
+
--- makefile
+++ makefile
@@ -1,14 +1,23 @@
 #Do not edit the contents of this file.
-CC = gcc
-CFLAGS = -Werror -Wall -g -std=gnu99
-LDFLAGS = -lrt -lpthread
-TARGET = allocation
-OBJFILE = allocation.o
-all: $(TARGET)
-allocation: allocation.c
+# the compiler: gcc for C program, define as g++ for C++
+ CC = gcc
+
+  # compiler flags:
+  #  -g    adds debugging information to the executable file
+  #  -Wall turns on most, but not all, compiler warnings
+ CFLAGS  = -Werror -Wall -g -std=gnu99
+ LDFLAGS = -lrt -lpthread
+
+  # the build target executable:
+ TARGET = allocation
+ OBJFILE = allocation.o
+
+ all: $(TARGET)
+
+$(TARGET): $(TARGET).c
 	$(CC) $(CFLAGS) -o allocation allocation.c $(LDFLAGS)
 
-runq2: allocation
+run: allocation
 	./allocation
 clean:
-	rm -f $(OBJFILES) $(TARGET) *~ 
+	rm -f $(OBJFILES) $(TARGET) *~
